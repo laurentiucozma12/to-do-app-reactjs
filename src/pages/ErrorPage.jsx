d49de673cf3974da8bd58d6f0107faf774f50f7b
Lau <laurentiucozma12@gmail.com>
2024-04-29 18:16:57 +0300

    error page

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,15 +1,14 @@
-import { useRouteError } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
+  const location = useLocation();
 
   return (
-    <div id="error-page">
+    <div id="error-page" className="text-center">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>Could not find {location.pathname}</i>
       </p>
     </div>
   );
